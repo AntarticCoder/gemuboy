ec93c4af7369b6b106c0f163308016d1bf0a44c4
Nitish Prakash <nitishpraks2009@gmail.com>
2026-06-22 01:09:29 -0400

    WIP: Implement more instruction for boot rom
    This commit implements 9 more instructions used in the boot room, and
    help push the code base closer to a PPU implementation. Register F has
    been change from a 'u8' to it's own struct. This commit also adds
    support for 16 bit instructions and a few instructions from that group.
    More groups of ram are also added. Some bugs with addressing and
    previous instructions are fixed, and some more instructions are likely
    to be needed to fixed as well in the future.

--- src/bus.rs
+++ src/bus.rs
@@ -1,33 +1,45 @@
 pub struct Bus {
     // rom: [u8; 0x3FFF],
+    boot_rom: [u8; 0x00FF - 0x0000 + 1],
     rom: Vec<u8>,
-    wram: [u8; 0xDFFF - 0xC000],
-    vram_tile: [u8; 0x9FFF - 0x8000],
+    wram: [u8; 0xDFFF - 0xC000 + 1],
+    vram_tile: [u8; 0x9FFF - 0x8000 + 1],
+    io_registers: [u8; 0xFF7F - 0xFF00 + 1],
+    hram: [u8; 0xFFFE - 0xFF80 + 1]
 }
 
 impl Bus {
     pub fn new(rom: Vec<u8>) -> Self {
         let bus = Bus {
+            boot_rom: rom.clone().try_into().unwrap(),
             rom: rom,
-            wram: [0; 0xDFFF - 0xC000],
-            vram_tile: [0; 0x9FFF - 0x8000]
+            wram: [0; 0xDFFF - 0xC000 + 1],
+            vram_tile: [0; 0x9FFF - 0x8000 + 1],
+            io_registers: [0; 0xFF7F - 0xFF00 + 1],
+            hram: [0; 0xFFFE - 0xFF80 + 1]
         };
         bus
     }
 
     pub fn read8(&self, addr: u16) -> u8 {
         match addr {
-            0x0000..=0x3FFF => self.rom[addr as usize],
+            0x0000..=0x00FF => self.boot_rom[addr as usize],
+            0x0100..=0x3FFF => self.rom[(addr - 0x0100) as usize],
             0x8000..=0x9FFF => self.vram_tile[(addr - 0x8000) as usize],
+            0xFF00..=0xFF7F => self.io_registers[(addr - 0xFF00) as usize],
+            0xFF80..=0xFFFE => self.hram[(addr - 0xFF80) as usize],
             _ => 0,
         }
     }
 
     pub fn write8(&mut self, addr: u16, data: u8) {
         match addr {
-            0x0000..=0x3FFF => panic!("Invalid write of data:[{}] to address:[{}] in rom", data, addr),
+            0x0000..=0x00FF => panic!("Invalid write of data:[{}] to address:[{}] in boot rom", data, addr),
+            0x0100..=0x3FFF => panic!("Invalid write of data:[{}] to address:[{}] in rom", data, addr),
             0xC000..=0xDFFF => self.wram[(addr - 0xC000) as usize] = data,
             0x8000..=0x9FFF => self.vram_tile[(addr - 0x8000) as usize] = data,
+            0xFF00..=0xFF7F => self.io_registers[(addr - 0xFF00) as usize] = data,
+            0xFF80..=0xFFFE => self.hram[(addr - 0xFF80) as usize] = data,
             _ => panic!("Failed write. \nData: [{}]\nAddress:[{}]", data, addr)
         }
     }
--- src/cpu.rs
+++ src/cpu.rs
@@ -1,8 +1,15 @@
 use crate::bus::Bus;
 
+struct FlagRegister {
+    pub zero: bool,
+    pub sub: bool,
+    pub half_carry: bool,
+    pub carry: bool
+}
+
 pub struct CPU {
     a: u8,
-    f: u8,
+    f: FlagRegister,
     b: u8,
     c: u8,
     d: u8,
@@ -24,13 +31,31 @@ const OPCODE_LUT: [Opcode; 256]  = {
     let mut table = [CPU::unimplemented_instruction as Opcode; 256];
 
     table[0x00] = CPU::nop;
+    table[0x0E] = CPU::ld_c_d8;
+    table[0x11] = CPU::ld_de_d16;
+    table[0x1A] = CPU::ld_a_de;
+    table[0x20] = CPU::jr_nz_s8;
     table[0x21] = CPU::ld_hl_d16;
     table[0x22] = CPU::ld_hl_plus_a;
+    table[0x28] = CPU::jr_z_s8;
     table[0x31] = CPU::ld_sp_d16;
+    table[0x47] = CPU::ld_b_a;
+    table[0x71] = CPU::ld_hl_c;
     table[0x3E] = CPU::ld_a_d8;
     table[0xC3] = CPU::jp_a16;
+    table[0xCD] = CPU::call_a16;
+    table[0xE0] = CPU::ld_a8_a;
     table[0xEA] = CPU::ld_a16_a;
     table[0xF3] = CPU::di;
+
+    table
+};
+
+const OPCODE_CB_LUT: [Opcode; 256]  = {
+    let mut table = [CPU::unimplemented_instruction as Opcode; 256];
+
+    table[0x20] = CPU::sla_b;
+    table[0x6C] = CPU::bit_5_h;
 
     table
 };
@@ -39,7 +64,12 @@ impl CPU {
     pub fn new() -> Self {
         CPU {
             a: 0,
-            f: 0,
+            f: FlagRegister {
+                zero: false,
+                sub: false,
+                half_carry: false,
+                carry: false,
+            },
             b: 0,
             c: 0,
             d: 0,
@@ -120,12 +150,100 @@ impl CPU {
             None => panic!("HL register increment overflowed!"),
         }
 
-        let hl = u16::to_le_bytes(addr);
-        self.l = hl[0];
-        self.h = hl[1];
+        let [lo, hi] = u16::to_le_bytes(addr.wrapping_add(1));
+        self.l = lo;
+        self.h = hi;
         println!("LD (HL+), A")
     }
 
+    pub fn bit_5_h(&mut self, bus: &mut Bus) {
+        let bit = self.h & (1 << 5) != 0;
+        // self.f.zero = bit;
+        self.f.zero = !bit;
+
+        println!("BIT 5, H")
+    }
+
+    pub fn jr_z_s8(&mut self, bus: &mut Bus) {
+        let offset = self.fetch8(bus) as i8;
+        if self.f.zero {
+            self.pc = ((self.pc as i16) + (offset as i16)) as u16;
+        }
+        println!("JR Z, S8")
+    }
+
+    pub fn ld_a8_a(&mut self, bus: &mut Bus) {
+        let addr = 0xFF00 + self.fetch8(bus) as u16;
+        bus.write8(addr, self.a);
+        println!("LD (A8), A")
+    }
+
+    pub fn ld_de_d16(&mut self, bus: &mut Bus) {
+        let data = self.fetch16(bus);
+
+        self.e = data.to_le_bytes()[0];
+        self.d = data.to_le_bytes()[1];
+        println!("LD DE, D16")
+    }
+
+    pub fn ld_a_de(&mut self, bus: &mut Bus) {
+        let addr = u16::from_le_bytes([self.e, self.d]);
+        self.a = bus.read8(addr);
+
+        println!("LD A, DE")
+    }
+
+    pub fn ld_hl_c(&mut self, bus: &mut Bus) {
+        let addr = u16::from_le_bytes([self.l, self.h]);
+        bus.write8(addr, self.c);
+
+        println!("LD HL, C")
+    }
+
+    pub fn ld_b_a(&mut self, bus: &mut Bus) {
+        self.b = self.a;
+        println!("LD B, A")
+    }
+
+    pub fn call_a16(&mut self, bus: &mut Bus) {
+        println!("SP: {}", self.sp);
+
+        let func_addr = self.fetch16(bus);
+
+        let [lo, hi] = u16::to_le_bytes(self.pc);
+        self.sp = self.sp.wrapping_sub(1);
+        bus.write8(self.sp, hi);
+        self.sp = self.sp.wrapping_sub(1);
+        bus.write8(self.sp, lo);
+
+        self.pc = func_addr;
+
+        println!("CALL A16")
+    }
+
+    pub fn ld_c_d8(&mut self, bus: &mut Bus) {
+        self.c = self.fetch8(bus);
+        println!("LD C, D8")
+    }
+
+    pub fn jr_nz_s8(&mut self, bus: &mut Bus) {
+        if !self.f.zero {
+            let offset = self.fetch8(bus) as i8;
+            self.pc = ((self.pc as i16) + (offset as i16)) as u16;
+        }
+
+        println!("JR NZ, S8")
+    }
+
+    pub fn sla_b(&mut self, bus: &mut Bus) {
+        let mask = 1 << 7;
+        let carry = (mask & self.b) > 0;
+        self.f.carry = carry;
+
+        self.b = self.b << 1;
+
+        println!("SLA B")
+    }
 
     pub fn decode_and_execute(&mut self, bus: &mut Bus) {
         if self.halt {
@@ -134,6 +252,14 @@ impl CPU {
 
         let opcode = bus.read8(self.pc);
         self.pc += 1;
+
+        if opcode == 0xCB {
+            println!("CB PREFIX");
+            let cb_opcode = self.fetch8(bus);
+            OPCODE_CB_LUT[cb_opcode as usize](self, bus);
+            return;
+        }
+
         OPCODE_LUT[opcode as usize](self, bus);
     }
 }   
